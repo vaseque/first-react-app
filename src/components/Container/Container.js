eac20e21a966c8daac55a4452d29770cafeec792
artur wasek <vaseque@yahoo.co.uk>
2020-04-06 23:01:22 +0200

    Add container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Container.scss';
+import PropTypes from 'prop-types';
+
+const Container = props => (
+
+  <div className={styles.component}>
+    {props.children}
+  </div>
+);
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Container;
