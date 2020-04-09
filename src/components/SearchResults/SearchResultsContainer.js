76642c7552f4520707c915c31d0c7b281471b371
artur wasek <vaseque@yahoo.co.uk>
2020-04-09 17:53:18 +0200

    Add SearchResult v3

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -5,11 +5,9 @@ import { getCardsForSearchResults } from '../../redux/cardsRedux.js';
 const mapStateToProps = (state, props) => {
 
   const inputString = props.match.params.history;
-  const filteredCards = state.cards.filter(card => new RegExp(inputString, 'i').test(card.title));
-  const cardParams = filteredCards[0] || {};
 
   return {
-    ...cardParams,
+
     cards: getCardsForSearchResults(state, inputString),
   };
 };
